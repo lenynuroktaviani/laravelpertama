b66d86f74e40d09e96dddfffdd9d13c788c0531d
lenynuroktaviani <lenyoktaviani1210@gmail.com>
2021-01-18 22:12:46 +0700

    8 REST API GET POST

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CobaController;
+use App\Http\Controllers\Api\GroupsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('', [CobaController::class, 'index']);
+Route::resources([
+    'friends' => CobaController::class,
+    'groups' => GroupsController::class,
+]);
